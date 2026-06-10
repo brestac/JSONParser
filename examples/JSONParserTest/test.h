6502df8c9f7b57cf87b45bbfdda94e9b65550ea5
romain <brestac@icloud.com>
2026-06-10 09:19:11 +0200

    Fix arduino test

--- examples/JSONParserTest/test.h
+++ examples/JSONParserTest/test.h
@@ -325,13 +325,13 @@ struct FeatureCollection : public JSONObject {
 };
 
 struct FeatureSansGeometry : public JSONObject {
-  std::string_view type = "";
+  char type[32] = { 0 };
   Properties properties;
   JSON_SERIALIZE_IMPL(type, properties);
 };
 
 struct FeatureCollectionSansGeometry : public JSONObject {
-  char type[32] = { 0 };
+  std::string_view type = "";
   std::vector<FeatureSansGeometry> features;
   JSON_SERIALIZE_IMPL(type, features);
 };
