e1b44d3a2d0debb8a6a50014fc4ade487f1e9413
Romain Brestac <cacaodev@gmail.com>
2026-06-27 10:49:02 +0000

    Change callback api to [](JSONKey&, JSONValue&, JSON::SKIP& skip)
    skip can be set in the callback to JSON::SKIP::STOP (same as stop = true) or JSON::SKIP::END that forces the parser to go to the end of the current array or object.

--- src/JSONCallbackObject.h
+++ src/JSONCallbackObject.h
@@ -7,13 +7,23 @@
 #include "ParseResult.h"
 #include "StreamCursor.h"
 
+NAMESPACE_JSON_BEGIN
+
+enum SKIP : uint8_t {
+    NONE = 0,
+    END = 1,
+    STOP = 2
+};
+
+NAMESPACE_JSON_END
+
 using JSONCallback =
-    std::function<void(const JSONKey &, const JSONValue &, bool &)>;
+    std::function<void(const JSONKey &, const JSONValue &, JSON::SKIP &)>;
 
 struct JSONCallbackObject {
   JSONCallback callback;
   JSONKey key;
-  bool stop;
+  JSON::SKIP skip = JSON::SKIP::NONE;
 
   JSON::ParseResult fromJSON(const char *name, JSON::StreamCursor &cursor);
   JSON::ParseResult fromJSON(const char *name, const JSON::PointerCursorReader &cursor);
@@ -23,21 +33,21 @@ struct JSONCallbackObject {
   }
 
   JSONCallbackObject(JSONCallback callback, JSONKey& key)
-      : callback(callback), key(key), stop(false) {
+      : callback(callback), key(key), skip(JSON::SKIP::NONE) {
     JSON_DEBUG_INFO("JSONCallbackObject created\n");
   }
 
   template <size_t N>
   JSONCallbackObject(JSONCallback callback, const char (&keyStr)[N])
-      : callback(callback), key(keyStr), stop(false) {
+      : callback(callback), key(keyStr), skip(JSON::SKIP::NONE) {
     JSON_DEBUG_INFO("JSONCallbackObject created from string\n");
   }
 
   void run(const JSONValue &value) {
     if (callback) {
       JSON_DEBUG_INFO("JSONCallbackObject running callback with key %.*s _array_index=%d\n", (int)key.length(), key.data(), key.getArrayIndex());
-      callback(key, value, stop);
-      if (stop) {
+      callback(key, value, skip);
+      if (skip == JSON::SKIP::STOP) {
         JSON_DEBUG_INFO("JSONCallbackObject stopped\n");
       }
     }
@@ -45,7 +55,6 @@ struct JSONCallbackObject {
 
   void setArrayIndex(int16_t anIndex) {
     JSON_DEBUG_INFO("JSONCallbackObject setArrayIndex %d\n", anIndex);
-    this->key.print();
     this->key.setArrayIndex(anIndex);
   }
 
