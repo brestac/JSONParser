6277aa1f0294426eecff9497d70f938e30730a5d
Romain Brestac <cacaodev@gmail.com>
2026-06-29 15:24:59 +0000

    [replit] update test

--- tests/desktop.cpp
+++ tests/desktop.cpp
@@ -24,29 +24,12 @@
 #include "../include/rapidjson/writer.h"
 
 #include "../examples/JSONParserTest/test.h"
-#include "../src/fast_float.h"
 
 int main() {
-//   bool all_passed = run_tests();
-// #ifdef JSON_DEBUG_MEM
-//   std::printf("GLOBAL_STRING_POOL_SIZE=%zu\n", JSON::GLOBAL_STRING_POOL_SIZE);
-//   std::printf("MAX_GLOBAL_PARSER_SIZE=%zu\n", JSON::MAX_GLOBAL_PARSER_SIZE);
-// #endif
-  // Serial.printf("sizeof JSONParserBase<StreamCursor>: %u\n", sizeof(JSON::JSONParserBase<JSON::StreamCursor, true, JSONCallbackObject>));
-  // return all_passed ? 0 : 1;
-  // return 0;
-  //test fast_float
-  // const char* p = "123.456 xx";
-  // double d;
-  // auto r = fast_float::from_chars(p, p + strlen(p), d);
-
-  // if (r.ec == std::errc()) {
-  //   std::cout << "success" << std::endl;
-  // } else {
-  //   std::cout << "fail" << std::endl;
-  // }
-  
-  // std::cout << d << std::endl;
-  // std::cout << (r.ptr - p) << std::endl;
-  return 0;
+   bool all_passed = run_tests();
+#ifdef JSON_DEBUG_MEM
+  std::printf("GLOBAL_STRING_POOL_SIZE=%zu\n", JSON::GLOBAL_STRING_POOL_SIZE);
+  std::printf("MAX_GLOBAL_PARSER_SIZE=%zu\n", JSON::MAX_GLOBAL_PARSER_SIZE);
+#endif
+  return all_passed ? 0 : 1;
 }
